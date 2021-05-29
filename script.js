698306ef653b68772dffacb1a639c220500102dc
marcos-vinicios13 <80989826+marcos-vinicios13@users.noreply.github.com>
2021-05-29 10:51:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const anoemquetamos =parseInt("2021")
+const nome=(prompt("digite seu nome"))
+const idade =parseInt(prompt("digite o eno em que vc naceu"))
+
+alert(` ola,o seu nome e ${nome} voce tem ${anoemquetamos- idade } anos`)
